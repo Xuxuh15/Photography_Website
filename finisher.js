e8dff1a66b1cbf4176abf9681223402ad70d4407
Xuxuh <jk.hilton@hotmail.com>
2023-04-18 16:48:20 +0200

    First commit

---
+++ finisher.js
@@ -0,0 +1,37 @@
+new FinisherHeader({
+    "count": 4,
+    "size": {
+      "min": 1200,
+      "max": 1500,
+      "pulse": 0.1
+    },
+    "speed": {
+      "x": {
+        "min": 0,
+        "max": 0.2
+      },
+      "y": {
+        "min": 0,
+        "max": 0.2
+      }
+    },
+    "colors": {
+      "background": "#ab51ff",
+      "particles": [
+        "#ff636b",
+        "#fbff56",
+        "#ffc648",
+        "#ff77ff"
+      ]
+    },
+    "blending": "lighten",
+    "opacity": {
+      "center": 0.8,
+      "edge": 0.2
+    },
+    "skew": -2,
+    "shapes": [
+      "c",
+      "t"
+    ]
+  });
